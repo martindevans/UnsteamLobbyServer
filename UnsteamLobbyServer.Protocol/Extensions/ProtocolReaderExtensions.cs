afbfe30ea0cc9c0260321dcc5c527f7f61cf2304
Martin Evans <martindevans@gmail.com>
2026-04-24 17:07:36 +0100

    Extensions byref

--- UnsteamLobbyServer.Protocol/Extensions/ProtocolReaderExtensions.cs
+++ UnsteamLobbyServer.Protocol/Extensions/ProtocolReaderExtensions.cs
@@ -4,14 +4,14 @@ namespace UnsteamLobbyServer.Protocol.Extensions
 {
     public static class ProtocolReaderExtensions
     {
-        public static TPacket? ReadBaseWebsocketMessageToServer<TReader, TPacket>(this TReader reader)
+        public static TPacket? ReadBaseWebsocketMessageToServer<TReader, TPacket>(ref this TReader reader)
             where TReader : struct, IByteReader
             where TPacket : BaseWebsocketMessageToServer
         {
             return (TPacket?)BaseWebsocketMessageToServer.Deserialize(ref reader);
         }
 
-        public static TPacket? ReadBaseWebsocketMessageToClient<TReader, TPacket>(this TReader reader)
+        public static TPacket? ReadBaseWebsocketMessageToClient<TReader, TPacket>(ref this TReader reader)
             where TReader : struct, IByteReader
             where TPacket : BaseWebsocketMessageToClient
         {
--- UnsteamLobbyServer.Protocol/Extensions/ProtocolWriterExtensions.cs
+++ UnsteamLobbyServer.Protocol/Extensions/ProtocolWriterExtensions.cs
@@ -4,13 +4,13 @@ namespace UnsteamLobbyServer.Protocol.Extensions;
 
 public static class ProtocolWriterExtensions
 {
-    public static void Write<TWriter>(this TWriter writer, BaseWebsocketMessageToServer packet)
+    public static void Write<TWriter>(ref this TWriter writer, BaseWebsocketMessageToServer packet)
         where TWriter : struct, IByteWriter
     {
         packet.Serialize(ref writer);
     }
 
-    public static void Write<TWriter>(this TWriter writer, BaseWebsocketMessageToClient packet)
+    public static void Write<TWriter>(ref this TWriter writer, BaseWebsocketMessageToClient packet)
         where TWriter : struct, IByteWriter
     {
         packet.Serialize(ref writer);
